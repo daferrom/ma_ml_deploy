6dfe77985a547bbcf34a33ca930d3a3ddcfd41b1
Diego Ferro <daferrom@gmail.com>
2024-12-18 19:48:24 -0500

    feat: Added error hanfdling to API

--- main.py
+++ main.py
@@ -12,21 +12,17 @@ final_model = tf.keras.models.load_model("best_efficientnet_model_V3.keras")
 
 @app.post("/predict-land-use/")
 async def predict_land_use(file: UploadFile = File(...)):
-    image = Image.open(io.BytesIO(await file.read()))
-    
-    # Preprocesar la imagen según lo requiera EfficientNet
-    # Comentado por ahora
-    image = image.resize((224, 224))  # Tamaño típico para EfficientNet
-    image = image.convert("RGB")     # Asegurar que es RGB
-    image_array = np.array(image) / 255.0  # Normalización
-    # Expandir las dimensiones de la imagen para simular un lote de tamaño 1
-    
-    # Expandir las dimensiones para simular un batch
-    img_batch = np.expand_dims(image_array, axis=0)
-    
-    # Pasar la imagen al modelo (esto depende de cómo hayas configurado EfficientNet)
-    prediction_probability = final_model.predict(img_batch)
-    
-    predicted_class_number = np.argmax(prediction_probability, axis=1)[0]  # Obtener la clase predicha
-    
-    return JSONResponse({"prediction": predicted_class_number})
+    try:
+        image = Image.open(io.BytesIO(await file.read()))
+        image = image.resize((224, 224))  # Tamaño típico para EfficientNet
+        image = image.convert("RGB")      # Asegurar que es RGB
+        image_array = np.array(image) / 255.0  # Normalización
+        img_batch = np.expand_dims(image_array, axis=0)
+        
+        # Hacer la predicción
+        prediction_probability = final_model.predict(img_batch)
+        predicted_class_number = np.argmax(prediction_probability, axis=1)[0]
+        
+        return JSONResponse({"prediction": predicted_class_number})
+    except Exception as e:
+        return JSONResponse({"error": f"Hubo un problema procesando la imagen: {str(e)}"}, status_code=500)
