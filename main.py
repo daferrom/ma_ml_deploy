1f56204102852d478db3a63a4f914702527f76d4
Diego Ferro <daferrom@gmail.com>
2024-12-18 19:07:56 -0500

    feat: Updated deploy script

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi.responses import JSONResponse
-import joblib
 import io
 import numpy as np
 from PIL import Image
@@ -9,7 +8,7 @@ from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
-final_model = joblib.load("model.joblib")
+final_model = tf.keras.models.load_model("best_efficientnet_model_V3.keras")
 
 @app.post("/predict-land-use/")
 async def predict_land_use(file: UploadFile = File(...)):
@@ -17,12 +16,13 @@ async def predict_land_use(file: UploadFile = File(...)):
     
     # Preprocesar la imagen según lo requiera EfficientNet
     # Comentado por ahora
-    # image = image.resize((224, 224))  # Tamaño típico para EfficientNet
-    # image = image.convert("RGB")     # Asegurar que es RGB
-    # image_array = np.array(image) / 255.0  # Normalización
+    image = image.resize((224, 224))  # Tamaño típico para EfficientNet
+    image = image.convert("RGB")     # Asegurar que es RGB
+    image_array = np.array(image) / 255.0  # Normalización
     # Expandir las dimensiones de la imagen para simular un lote de tamaño 1
     
-    img_batch = tf.expand_dims(image, axis=0)
+    # Expandir las dimensiones para simular un batch
+    img_batch = np.expand_dims(image_array, axis=0)
     
     # Pasar la imagen al modelo (esto depende de cómo hayas configurado EfficientNet)
     prediction_probability = final_model.predict(img_batch)
